ead9134e69d2ba180079906280979c0464cd27d1
csperlich <csperlich@gmail.com>
2014-10-16 18:37:40 -0400

    Reorganize folders. Add Table and floor. Add Cameras and scripts to control them. Scale all objects by 10 for better collision detection and no overlap. Because of scale up, rigidbody forces and gravity will probably need to be scaled as well. Add audio script for ball (it's not very good).

---
+++ Roulette/Assets/Scripts/BallAudio.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallAudio : MonoBehaviour {
+
+	public float audioClipSpeed = 10.0f;
+	public float pitchMin;
+	public float pitchMax;
+	public float volMin;
+	public float volMax;
+
+
+	void OnCollisionEnter(Collision collision) {
+		audio.Play ();
+	}
+
+	void Update() {
+		float p = rigidbody.velocity.magnitude / audioClipSpeed;
+		audio.pitch = Mathf.Clamp (p, pitchMin, pitchMax);
+		audio.volume = Mathf.Clamp (p, volMin, volMax);
+
+	}
+
+	/*void OnCollisionExit(Collision collision) {
+		audio.Stop();
+	}*/
+}
---
+++ Roulette/Assets/Scripts/TrackingCamera.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackingCamera : MonoBehaviour {
+	public GameObject target;
+
+	void LateUpdate() {
+		transform.LookAt(target.transform);
+	}
+}
