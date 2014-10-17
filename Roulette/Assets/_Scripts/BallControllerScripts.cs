64e7f8de90a95df2f56a827f23ccf56fb76a6305
Eddie Gurnee <pegurnee@gmail.com>
2014-10-17 19:06:38 -0400

    trying to get some ball reaction

---
+++ Roulette/Assets/_Scripts/BallControllerScripts.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControllerScripts : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void OnCollisionEnter (Collision collision)
+		{
+		print ("gah");
+			this.rigidbody.AddForce (collision.relativeVelocity * collision.relativeVelocity.magnitude * 100 * Time.deltaTime);
+		}
+}
