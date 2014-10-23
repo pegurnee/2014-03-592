79e966b593bd24febaa66613e65d9b029942e0d3
Eddie Gurnee <pegurnee@gmail.com>
2014-10-23 02:19:35 -0400

    betting almost instituted

--- Roulette/Assets/_Scripts/BallControllerScripts.cs
+++ Roulette/Assets/_Scripts/BallControllerScripts.cs
@@ -3,58 +3,75 @@ using System.Collections;
 
 public class BallControllerScripts : MonoBehaviour
 {
-	public Transform theBowl;
-	public float launchSpeed;
+		public Transform theBowl;
+		public float launchSpeed;
+		private bool ballIsStopped;
+		private int framesInTrigger;
+		private int pocket;
 
-	private bool ballIsStopped;
-	private int framesInTrigger;
+		void Start ()
+		{
+				ballIsStopped = false;
+				framesInTrigger = 0;
+		}
 
-	void Start ()
-	{
-		ballIsStopped = false;
-		framesInTrigger = 0;
-	}
+		void Update ()
+		{
+				if (Input.GetKeyDown ("space"))
+						launchBall ();
+		}
 
-	void Update()
-	{
-		if (Input.GetKeyDown("space"))
-			launchBall();
-	}
+		void OnTriggerEnter (Collider collider)
+		{
+				framesInTrigger = 0;
+		}
 
-	void OnTriggerEnter (Collider collider)
-	{
-		framesInTrigger = 0;
-	}
+		void OnTriggerStay (Collider collider)
+		{
+				if (++framesInTrigger > 500 && !this.ballIsStopped) {
+						ballIsStopped = true;
+						string name = collider.gameObject.name;
+						string num = name.Substring ("Pocket".Length);
+						this.pocket = num.Equals ("00") ? 31 : int.Parse (num);
 
-	void OnTriggerStay (Collider collider)
-	{
-		if (++framesInTrigger > 500)
-			ballIsStopped = true;
-	}
+						Debug.Log (collider.gameObject.name);
+						Debug.Log (collider.gameObject.name.Substring ("Pocket".Length));
+						Debug.Log (this.pocket);
+				}
+		}
 
-	void launchBall() 
-	{
-		//randomly place the ball along the inner edge of the bowl
-		rigidbody.isKinematic = true;
-		float degrees = Random.Range(0, 360);
-		Vector3 radius = new Vector3(0.0f, 0.0f, 7.5f) + new Vector3(0.0f, 2.5f, 0.0f);
-		transform.position = theBowl.position + radius;
-		transform.RotateAround(theBowl.position, Vector3.up, degrees);
+		public void launchBall ()
+		{
+				this.ballIsStopped = false;
+				this.framesInTrigger = 0;
+				this.pocket = -1;
 
-		//launch the ball along the tangent line 
-		rigidbody.isKinematic = false;
-		rigidbody.velocity = Vector3.zero;
-		Vector3 tangent = Vector3.Cross (transform.position, Vector3.up).normalized;
-		rigidbody.AddForce(tangent * launchSpeed, ForceMode.Impulse);
-	}
+				//randomly place the ball along the inner edge of the bowl
+				rigidbody.isKinematic = true;
+				float degrees = Random.Range (0, 360);
+				Vector3 radius = new Vector3 (0.0f, 0.0f, 7.5f) + new Vector3 (0.0f, 2.5f, 0.0f);
+				transform.position = theBowl.position + radius;
+				transform.RotateAround (theBowl.position, Vector3.up, degrees);
 
-	void OnTriggerExit (Collider collider)
-	{
-		framesInTrigger = 0;
-	}
+				//launch the ball along the tangent line 
+				rigidbody.isKinematic = false;
+				rigidbody.velocity = Vector3.zero;
+				Vector3 tangent = Vector3.Cross (transform.position, Vector3.up).normalized;
+				rigidbody.AddForce (tangent * launchSpeed, ForceMode.Impulse);
+		}
 
-	public bool isStopped ()
-	{
-		return ballIsStopped;
-	}
+		void OnTriggerExit (Collider collider)
+		{
+				framesInTrigger = 0;
+		}
+
+		public bool isStopped ()
+		{
+				return ballIsStopped;
+		}
+
+		public int getPocket ()
+		{
+				return this.pocket;
+		}
 }
--- Roulette/Assets/_Scripts/WheelControllerScript.cs
+++ Roulette/Assets/_Scripts/WheelControllerScript.cs
@@ -3,27 +3,35 @@ using System.Collections;
 
 public class WheelControllerScript : MonoBehaviour
 {
-	public GameObject theBall;
+		public GameObject theBall;
+		public GameObject theGameController;
+		private float speed;
+		private BallControllerScripts ballScript;
+		private GUIController guiScript;
 
-	private float speed;
-	private BallControllerScripts ballScript;
+		// Use this for initialization
+		void Start ()
+		{
+				this.initSpeed ();
+				this.ballScript = theBall.GetComponent<BallControllerScripts> ();
+				this.guiScript = this.theGameController.GetComponent<GUIController> ();
+		}
 
-	// Use this for initialization
-	void Start ()
-	{
-		this.speed = 100;
-		this.ballScript = theBall.GetComponent<BallControllerScripts> ();
-	}
+		void FixedUpdate ()
+		{	
+				this.transform.eulerAngles += new Vector3 (0.0f, 0.0f, .002f * this.speed);
 
-	void FixedUpdate ()
-	{	
-		this.transform.eulerAngles += new Vector3 (0.0f, 0.0f, .002f * this.speed);
+				if (ballScript.isStopped () == true) {
+						this.speed -= .05f;
+				}
+				if (this.speed <= 0.0f) {
+						this.speed = 0.0f;
+						this.guiScript.newBet ();
+				}
+		}
 
-		if (ballScript.isStopped () == true)
-			this.speed -= .05f;
-		if (this.speed <= 0.0f)
-			this.speed = 0.0f;
-
-	}
-
+		public void initSpeed ()
+		{
+				this.speed = 100;
+		}
 }
