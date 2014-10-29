5bdc8b935c86eca6150105dff4915cd50e01958c
Eddie Gurnee <pegurnee@gmail.com>
2014-10-28 23:52:53 -0400

    add initial roll_a_ball_expanded

---
+++ roll_a_ball_expanded/Assets/_scripts/PickupController.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupController : MonoBehaviour
+{
+		private bool canMove;
+		private float counterOfTimePassed;
+		private Vector3 movement;
+		private float limitForHowLongToMove;
+		private const float moveLimitLowerBound = 0.25f;
+		private const float moveLimitUpperBound = 1.50f;
+		private float speed;
+		private const float speedLimitLowerBound = 0.5f;
+		private const float speedLimitUpperBound = 2.0f;
+		private Vector3 initialPosition;
+
+		void Start ()
+		{
+				this.canMove = true;
+				this.MoveInRandomDirectionForRandomAmountOfTime ();
+				this.initialPosition = new Vector3 (this.gameObject.transform.position.x, 
+		                                   this.gameObject.transform.position.y, 
+		                                   this.gameObject.transform.position.z);
+		}
+	
+		void Update ()
+		{
+				if (this.canMove) {
+						this.transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+				} else {
+		
+						if (Input.GetKeyDown ("space")) {
+								this.ResetPosition ();
+						}
+				}
+		
+		}
+
+		void FixedUpdate ()
+		{
+
+				this.counterOfTimePassed += Time.deltaTime;
+				if (this.canMove) {
+						this.transform.Translate (this.movement * Time.deltaTime * this.speed, Space.World);
+		
+						if (this.counterOfTimePassed > this.limitForHowLongToMove) {
+								this.MoveInRandomDirectionForRandomAmountOfTime ();
+						} 
+				}
+		}
+
+		void MoveInRandomDirectionForRandomAmountOfTime ()
+		{
+				this.ChangeDirection ();
+				this.ComputeNewSpeed ();
+				this.ResetTiming ();
+		}
+	
+		private void ComputeNewSpeed ()
+		{
+				this.speed = Random.Range (PickupController.speedLimitLowerBound, PickupController.speedLimitUpperBound);
+		}
+	
+		private void ResetTiming ()
+		{
+				this.limitForHowLongToMove = Random.Range (PickupController.moveLimitLowerBound, PickupController.moveLimitUpperBound);
+				this.counterOfTimePassed = 0;
+		}
+
+		private void ChangeDirection ()
+		{
+				float moveHorizontal = Random.Range (-1.0f, 1.0f);
+				float moveVertical = Random.Range (-1.0f, 1.0f);
+		
+				this.movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		}
+
+		void KillOne (GameObject firstPickup, GameObject secondPickup)
+		{
+				if (firstPickup.GetInstanceID () < secondPickup.GetInstanceID ()) {
+						Destroy (firstPickup);
+				} else {
+						Destroy (secondPickup);
+				}
+
+		}
+
+		void ResetPosition ()
+		{
+				this.transform.position = this.initialPosition;
+				this.canMove = true;
+		}
+
+		void OnCollisionEnter (Collision other)
+		{
+				switch (other.gameObject.tag) {
+				case "player":
+						Destroy (this.gameObject);
+						break;
+				case "pickup":
+						this.KillOne (this.gameObject, other.gameObject);
+						break;
+				case "wall":
+						this.canMove = false;
+						break;
+				default:
+						break;
+				}
+		}
+}
---
+++ roll_a_ball_expanded/Assets/_scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public float jumpSpeed;
+	private float groundHeight;
+	
+	void Start ()
+	{
+		this.groundHeight = this.transform.position.y;
+	}
+	
+	void FixedUpdate ()
+	{
+		if (Input.GetKeyDown ("space") 
+		    && (this.transform.position.y <= this.groundHeight)
+		    ) {
+			this.rigidbody.AddForce (Vector3.up * this.jumpSpeed);
+		} else {
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+			
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			
+			this.rigidbody.AddForce (movement * this.speed * Time.deltaTime);
+		}
+	}
+	void OnTriggerEnter (Collider other)
+	{
+//		print ("here");
+//		print (other.gameObject.tag);
+		}
+}
