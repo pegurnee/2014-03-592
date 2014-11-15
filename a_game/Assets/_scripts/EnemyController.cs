9ebe4dbca1288d219e123c9fd6343c49daf3cfb0
Eddie Gurnee <pegurnee@gmail.com>
2014-11-14 21:34:42 -0500

    include a game

---
+++ a_game/Assets/_scripts/EnemyController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	GameObject player;
+	Vector3 movement;
+	private float limitForHowLongToMove;
+	private float moveLimitLowerBound = 0.25f;
+	private float moveLimitUpperBound = 1.50f;
+	private float speed;
+	private float speedLimitLowerBound = 0.5f;
+	private float speedLimitUpperBound = 2.0f; 
+	private float counterOfTimePassed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate() {
+		this.counterOfTimePassed += Time.deltaTime;
+		this.transform.Translate (this.movement * Time.deltaTime * this.speed, Space.World);
+		if (this.counterOfTimePassed > this.limitForHowLongToMove) {
+			this.MoveInRandomDirectionForRandomAmountOfTime ();
+		}
+	}
+
+	void MoveInRandomDirectionForRandomAmountOfTime ()
+	{
+		this.ChangeDirection ();
+		this.ComputeNewSpeed ();
+		this.ResetTiming ();
+	}
+	private void ComputeNewSpeed ()
+	{
+		this.speed = Random.Range (this.speedLimitLowerBound, this.speedLimitUpperBound);
+	}
+	private void ResetTiming ()
+	{
+		this.limitForHowLongToMove = Random.Range (this.moveLimitLowerBound, this.moveLimitUpperBound);
+		this.counterOfTimePassed = 0;
+	}
+	private void ChangeDirection ()
+	{
+		float moveHorizontal = Random.Range (-1.0f, 1.0f);
+		float moveVertical = Random.Range (-1.0f, 1.0f);
+		this.movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag.Equals ("Bullet")){
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ a_game/Assets/_scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+		private int speed = 5;
+		public GameObject prefab;
+		int direction; 
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+			if (Input.GetKey("up")) {
+				this.gameObject.transform.Translate((new Vector3(0f, 0f, 1f)) * Time.deltaTime * this.speed);
+				direction = 0;
+			} else if (Input.GetKey("down")) {
+				this.gameObject.transform.Translate((new Vector3(0f, 0f, -1f)) * Time.deltaTime * this.speed);
+				direction = 2;
+			} else if (Input.GetKey("right")) {
+				this.gameObject.transform.Translate((new Vector3(1f, 0f, 0f)) * Time.deltaTime * this.speed);
+				direction = 1;
+			} else if (Input.GetKey("left")) {
+				this.gameObject.transform.Translate((new Vector3(-1f, 0f, 0f)) * Time.deltaTime * this.speed);
+				direction = 3;
+			}
+
+			if (Input.GetKey ("w")) {
+				this.fire (0);
+			} else if (Input.GetKey ("a")) {
+				this.fire (3);
+			} else if (Input.GetKey ("s")) {
+				this.fire (2);
+			} else if (Input.GetKey ("d")) {
+				this.fire (1);
+			}
+		}
+
+		private void fire(int direction) {
+			GameObject clone;
+			clone = Instantiate(prefab, this.transform.position, Quaternion.identity) as GameObject;
+			clone.GetComponent<BulletController> ().setDirection (direction);
+		}
+
+		void FixedUpdate() {
+
+		}
+
+		public int getDirection () {
+			return direction;
+		}
+}
